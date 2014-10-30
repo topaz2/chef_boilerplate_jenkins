20c28c8c3aee63dbbb0c087fac063ac9f280464c
topaz2 <topaz2.3333@gmail.com>
2014-10-30 11:47:37 +0900

    Revert back to use bundle

--- files/default/tools/build/jenkins/app_deploy/main.sh
+++ files/default/tools/build/jenkins/app_deploy/main.sh
@@ -1,7 +1,3 @@
 #!/bin/bash -ex
 
-# Add chefdk path for the issue below
-# @see https://github.com/berkshelf/vagrant-berkshelf/issues/212
-export PATH='/opt/chefdk/bin:'$PATH
-
-knife solo cook localhost -N staging
+bundle ex knife solo cook localhost -N staging
