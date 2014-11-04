df8af8e73bdbe4b2a6963a0f5bc51b40d8c278a3
topaz2 <topaz2.3333@gmail.com>
2014-11-04 16:32:50 +0900

    Add chefdk path

--- files/default/tools/build/jenkins/app_cookbook/main.sh
+++ files/default/tools/build/jenkins/app_cookbook/main.sh
@@ -1,3 +1,7 @@
 #!/bin/bash -ex
 
+# Add chefdk path for the issue below
+# @see https://github.com/berkshelf/vagrant-berkshelf/issues/212
+export PATH='/opt/chefdk/bin:'$PATH
+
 kitchen test --destroy=always -c `ohai cpu/total`
